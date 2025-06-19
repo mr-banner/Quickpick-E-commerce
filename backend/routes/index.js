1e06dddee6d865540735afe6ac8a1b4a8ed07395
Prajwal M <prajwalmallapur123@gmail.com>
2025-06-19 18:08:46 +0530

    Subscribe email

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,7 @@ import cartRout from './cart.routes.js'
 import productRout from './product.routes.js'
 import userRout from './user.routes.js'
 import orderRout from './orders.routes.js'
+import subscribeRout from './subscribe.routes.js'
 
 const route = Router()
 
@@ -13,6 +14,7 @@ route.use('/api/v1/cart', cartRout)
 route.use('/api/v1/products', productRout)
 route.use('/api/v1/users', userRout)
 route.use('/api/v1/orders', orderRout)
+route.use('/api/v1/subscribe', subscribeRout)
 
 
 
